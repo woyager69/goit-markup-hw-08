0b70d86d8d1e18d986a708feacb5312fe6f373d3
Serhii Bulychov <woyager69@gmail.com>
2022-01-21 15:07:30 +0200

    buttons&modals

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,14 @@
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
-
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
 // (() => {
 //   const menuBtnRef = document.querySelector('.my-class1');
 //   const mobileMenuRef = document.querySelector('.my-class2');
@@ -23,15 +22,3 @@
 //     menuContainerRef.classList.toggle('is-open');
 //   });
 // })();
-
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
